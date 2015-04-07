68a16df390bf5286ff7e8737d38de8226d7b1326
Lobanion1 <lyle.O'Banion@omnicare.com>
2015-04-07 14:33:30 -0400

    Added Color By Region
    Created function to color facilities by pharm.

--- Server.r
+++ Server.r
@@ -4,19 +4,23 @@ library(dplyr)
 library(xlsx)
 library(shiny)
 
+Colorpallet <- c("1"="#595490","3"="#527525","6"="#A93F35","7"="#BA48AA","8"="Blue")
 
-FACILITY_CHAIN <- read.xlsx("C:/Users/lobanion/Desktop/Minnesota Study/FACILITY_CHAINmnopt.xlsx",1)
+FACILITY_CHAIN <- read.csv("L:/Cincinnati Routing Study/FACILITY_CHAINCINGEO.csv",header = TRUE)
 ##Subset the import to only show wanted columns
 FACILITY_CHAIN_TABLE <- FACILITY_CHAIN[ ,c("FACILITY_ID","FACILITY_NAME","ADDR1","CITY","STATE","TOTAL_BEDS","FACILITY_TYPE_GROUP")]
 
 ##Layered map with Map tile and circle based on route
 function(input, output) {
+  dirPal<- colorFactor(Colorpallet,FACILITY_CHAIN$COMPANY_ID_PRIMARY)
+  
+  
   map = leaflet() %>% 
     addTiles('http://{s}.tile.thunderforest.com/transport/{z}/{x}/{y}.png')%>% 
     addCircleMarkers(
       FACILITY_CHAIN$lon,
       FACILITY_CHAIN$lat,
-      color = "red",
+      color = dirPal(FACILITY_CHAIN$COMPANY_ID_PRIMARY),
       radius = 4,
    
 ##Popup information must be in a string format!!      
