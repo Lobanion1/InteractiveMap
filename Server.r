5e0da29746c79a72a6517ada591ca544a8b7fed7
Lobanion1 <lyle.O'Banion@omnicare.com>
2015-04-08 16:45:12 -0400

    CSV Import and Panel
    Added Panels and CSV import function

--- Server.r
+++ Server.r
@@ -6,14 +6,26 @@ library(shiny)
 
 Colorpallet <- c("1"="#595490","3"="#527525","6"="#A93F35","7"="#BA48AA","8"="Blue")
 
-FACILITY_CHAIN <- read.csv("L:/Cincinnati Routing Study/FACILITY_CHAINCINGEO.csv",header = TRUE)
+FACILITY_CHAIN <- read.csv("L:/Cincinnati Routing Study/FACILITY_CHAINNCAv2.csv",header = TRUE)
 ##Subset the import to only show wanted columns
 FACILITY_CHAIN_TABLE <- FACILITY_CHAIN[ ,c("FACILITY_ID","FACILITY_NAME","ADDR1","CITY","STATE","TOTAL_BEDS","FACILITY_TYPE_GROUP")]
 
 ##Layered map with Map tile and circle based on route
 function(input, output) {
+  
+  #Function to create color option
   dirPal<- colorFactor(Colorpallet,FACILITY_CHAIN$COMPANY_ID_PRIMARY)
   
+  output$contents <- renderTable({
+    inFile <- input$file1
+    
+    if (is.null(inFile))
+      return(NULL)
+    
+    read.csv(inFile$datapath, header=input$header, sep=input$sep, 
+             quote=input$quote)
+    FACILITY_CHAIN <- inFile
+    })
   
   map = leaflet() %>% 
     addTiles('http://{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}.png')%>% 
@@ -23,7 +35,7 @@ function(input, output) {
       color = dirPal(FACILITY_CHAIN$COMPANY_ID_PRIMARY),
       radius = 4,
    
-##Popup information must be in a string format!!      
+##Popup information must be in a string format.     
       popup= FACILITY_CHAIN$FACILITY_NAME)
       output$myMap = renderLeaflet(map)
 
--- ui.r
+++ ui.r
@@ -5,25 +5,51 @@ library(leaflet)
 header <- dashboardHeader(
   title = "Omnicare Inc."
 )
-
+shinyUI(navbarPage("Omnicare Inc. Supply Chain",
+tabPanel("Interactive Map",
 body <- dashboardBody(
   fluidRow(
     column(width = 9,
       box(width = NULL, solidHeader = TRUE,
-        leafletOutput('myMap', height = 500)
+        leafletOutput('myMap', height = 400)
       ),
   
       # Create a new row for the table.
       fluidRow(
         dataTableOutput(outputId="table")
-      )    
-    )  
-  )
+        )    
+          )  
+            )
+              )
+                 ),
+tabPanel("Data Management",
+         titlePanel("Uploading Files"),
+         sidebarLayout(
+           sidebarPanel(
+             fileInput('file1', 'Choose CSV File',
+                       accept=c('text/csv', 
+                                'text/comma-separated-values,text/plain', 
+                                '.csv')),
+             tags$hr(),
+             checkboxInput('header', 'Header', TRUE),
+             radioButtons('sep', 'Separator',
+                          c(Comma=',',
+                            Semicolon=';',
+                            Tab='\t'),
+                          ','),
+             radioButtons('quote', 'Quote',
+                          c(None='',
+                            'Double Quote'='"',
+                            'Single Quote'="'"),
+                          '"')
+           ),
+           mainPanel(
+             tableOutput('contents')
+           )
+         )
 )
-  
-
-
-
+)
+)
 dashboardPage(
   header,
   dashboardSidebar(disable = TRUE),
